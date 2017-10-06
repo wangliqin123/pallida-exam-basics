d9995748cf9f209d691b214aca9ec34bb77f7a1c
Lorand <szilagyi.lori@gmail.com>
2017-10-06 10:37:38 +0200

    first test passed in uniquecharacters

---
+++ uniquechars/UniqueChars/UniqueChars/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueChars
+{
+    public class Chars
+    {
+        public List<string> UniqueCharacters(string word)
+        {
+            List<string> letters = new List<string> { "c", "a", "t" };
+            return letters;
+        }
+    }
+}
---
+++ uniquechars/UniqueChars/UniqueChars/TestUniqueChars.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniqueChars;
+
+namespace UniqueChars
+{
+    [TestFixture]
+    class TestChars
+    {
+        Chars chars = new Chars();
+
+        [Test]
+        public void TestUniqueCharacters()       
+        {
+            string word = "cat";
+
+            List<string> output = new List<string>()
+            {
+                {"c"},
+                {"a"},
+                {"t"}
+            };
+
+            Assert.AreEqual(output, chars.UniqueCharacters(word));
+        }
+    }
+}
