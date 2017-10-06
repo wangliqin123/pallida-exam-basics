16578a59474f16e10a79cd402183b7ab687386b0
Lorand <szilagyi.lori@gmail.com>
2017-10-06 11:11:05 +0200

    class structure done in TakeMeToThe

---
+++ candyshop/TakeMeToThe/TakeMeToThe/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeMeToThe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CandyShop candyShop = new CandyShop();
+        }
+    }
+}
